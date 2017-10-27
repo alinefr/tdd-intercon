d539fda51ab06adc648d5662174450f4a69ba0f7
Aline Freitas <aline@alinefreitas.com.br>
2017-10-27 18:59:52 -0200

    Initial commit.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description 'Installs/Configures intercon'
 long_description 'Installs/Configures intercon'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
+depends 'docker'
+depends 'docker_compose'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,29 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Service docker installed, enabled and running.
+docker_service 'default' do
+  action [:create, :start]
+end
+
+# docker container running with nginx image.
+include_recipe 'docker_compose::installation'
+
+directory '/srv/docker/hello_world/html' do
+  recursive true
+end
+
+cookbook_file '/srv/docker/hello_world/docker-compose.yml' do
+  source 'docker-compose.yml'
+end
+
+docker_compose_application 'nginx' do
+  action :up
+  compose_files ['/srv/docker/hello_world/docker-compose.yml']
+end
+
+# Serve html page containing Hello World.
+cookbook_file '/srv/docker/hello_world/html/index.html' do
+  source 'index.html'
+end
